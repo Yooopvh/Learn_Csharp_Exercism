4660f70a6e5a3609db9a72d6c5c7a86b10a73d9d
vhidalgo <->
2024-08-08 13:11:10 +0200

    Resueltos 2 ejercicios de nivel medio (el problema de la mochila) y otro de optimización de coste de venta de libros

--- 16_Numbers.cs
+++ 16_Numbers.cs
@@ -1,12 +1,88 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Metadata.Ecma335;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Code
 {
-    internal class Class1
+    public class SpiralMatrix
     {
+        public static int[,] GetMatrix(int size)
+        {
+            int[,] solutionMatrix = new int[size,size];
+
+            int value = 1;
+            int index = 0;
+            bool ascending = true;
+
+            for ( int i = 0; i < size; i++ )
+            {
+                int maxValueIndex = size - 1 - (int)Math.Ceiling((double)i/2);
+                int minValueIndex = 0 + (int)Math.Floor((double)i/2);
+                // Horizontal values
+                if (ascending )
+                {
+                    for (int j = minValueIndex;  j <= maxValueIndex; j++ )
+                    {
+                        solutionMatrix[minValueIndex,j] = value;
+                        value++;
+
+                        //Check if the center has been reached
+                        if (minValueIndex == Math.Floor(((double) size+1)/2) && j == Math.Ceiling((double)size/2))
+                        {
+                            break;
+                        }
+                    }
+                }
+                else
+                {
+                    for (int j = maxValueIndex; j >= minValueIndex; j--)
+                    {
+                        solutionMatrix[size - minValueIndex-1, j] = value;
+                        value++;
+
+                        //Check if the center has been reached
+                        if (minValueIndex == Math.Floor(((double)size+1)/2) && j == Math.Ceiling((double)size/2))
+                        {
+                            break;
+                        }
+                    }
+                }
+
+                //Vertical values
+
+                maxValueIndex = size - 1 - (int)Math.Floor((double)(i+1)/2);
+                minValueIndex = 0 + (int)Math.Ceiling((double)(i+1)/2);
+                // Horizontal values
+                if (ascending)
+                {
+                    for (int j = minValueIndex; j <= maxValueIndex; j++)
+                    {
+                        solutionMatrix[j, (size-minValueIndex)] = value;
+                        value++;
+                    }
+                }
+                else
+                {
+                    for (int j = maxValueIndex; j >= minValueIndex; j--)
+                    {
+                        solutionMatrix[j, minValueIndex-1] = value;
+                        value++;
+                    }
+                }
+
+
+                //Switch
+                ascending = ascending ? false : true;
+
+            }
+
+            return solutionMatrix;
+        }
+
+       
     }
+
 }
---
+++ 17_FloatingPointNumbers.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Code
+{
+    public static class BookStore
+    {
+        public static decimal Total(IEnumerable<int> books)
+        {
+            double[] discounts = new double[] { 0, 0.05, 0.1, 0.2, 0.25 };
+
+            int[] numOfBooksOfEachType = new int[5];
+
+            foreach (int book in books)
+            {
+                numOfBooksOfEachType[book-1] +=1;
+            }
+
+            numOfBooksOfEachType = numOfBooksOfEachType.OrderByDescending(x => x).ToArray();
+
+            int NumOfGroups = numOfBooksOfEachType.Max();
+
+            int maxNumOfBookInGroup = 5 - numOfBooksOfEachType.Where(x => x == 0).Count();
+
+            int totalNumOfBooks = numOfBooksOfEachType.Sum();
+
+            int[] groups = new int[NumOfGroups];
+            int[] groupsShared = new int[NumOfGroups];
+            int[] groupsOptimum = new int[NumOfGroups];
+
+            decimal result = decimal.MaxValue;
+
+            foreach (int amountOfBooksOfEachType in numOfBooksOfEachType)
+            {
+                bool[] emptyGroups = new bool[NumOfGroups]; //Grupo al que se puede añadir un libro todavía
+                emptyGroups = emptyGroups.Select(x => x = true).ToArray();  //En promer lugar, el nuevo tipo de libro se puede añadir a cualquier grupo
+
+
+                for (int i = 0; i < amountOfBooksOfEachType; i++)   //para cada libro de un mismo tipo
+                {
+                    decimal auxResult = decimal.MaxValue;
+                    int bestIndex = -1;
+                    int[] groupsAux = (int[])groupsOptimum.Clone();
+
+                    for (int j = 0; j < NumOfGroups; j++)       //para cada grupo de libros
+                    {
+                        if (emptyGroups[j])     //Si un libro del mismo tipo no ha sido añadido todavía
+                        {
+                            groupsAux[j] +=1;
+
+                            if ((decimal)groupsAux.Select(x => x*8*(1-discounts[x = x ==0 ? 0 : x-1])).Sum() < auxResult)   //Evaluamos si añadir el libro a este grupo es mejor que al que teníamos guardado anteriormente como mejor
+                            {
+                                auxResult = (decimal)groupsAux.Select(x => x*8*(1-discounts[x = x ==0 ? 0 : x-1])).Sum();
+                                groupsOptimum = (int[])groupsAux.Clone();
+                                bestIndex = j;
+                            }
+
+                            groupsAux[j] -=1;
+                        }
+                    }
+
+                    emptyGroups[bestIndex] = false;
+                }
+            }
+
+            return (decimal)groupsOptimum.Select(x => x*8*(1-discounts[x-1])).Sum();
+        }
+    }
+}
