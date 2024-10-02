131979b62366f31fd786279940287957f6e28484
vhidalgo <->
2024-10-02 12:55:13 +0200

    Ejercicio teórico sobre ResourceCleanup (enseña un posco a usar IDisposable)

---
+++ 30_ResourceCleanup.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Code
+{
+    public enum States
+    {
+        Closed,
+        TransactionStarted,
+        DataWritten,
+        Invalid
+    }
+    public class Orm : IDisposable
+    {
+        private Database database;
+
+        public Orm(Database database)
+        {
+            this.database = database;
+        }
+
+        public void Begin() 
+        {
+            if (database.State == States.Closed)
+            {
+                database.State = States.TransactionStarted;
+            }else
+            {
+                Dispose();
+                throw new InvalidOperationException();
+            }
+        }
+
+        public void Write(string data)
+        {
+            if (database.State == States.TransactionStarted)
+            {
+                database.State = States.DataWritten;
+            }
+            else
+            {
+                Dispose();
+                throw new InvalidOperationException();
+            }
+        }
+
+        public void Commit()
+        {
+            if (database.State == States.DataWritten)
+            {
+                database.State = States.Closed;
+            }
+            else
+            {
+                Dispose();
+                throw new InvalidOperationException();
+            }
+        }
+
+        public void Dispose()
+        {
+            database.State = States.Closed;
+        }
+    }
+
+    public class Database
+    {
+        public States State = States.Closed;
+        
+    }
+}
