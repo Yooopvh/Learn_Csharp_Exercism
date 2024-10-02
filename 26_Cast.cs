7ecedf74a89b75c52f0168a4698663c403cc73f7
vhidalgo <->
2024-10-02 09:04:38 +0200

    Resueltos ejercicios de teoría de Overflow y Cast

---
+++ 26_Cast.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Code
+{
+    public class SecurityPassMaker
+    {
+        public string GetDisplayName(TeamSupport support)
+        {
+            if (support is Staff)
+            {
+                string message = support.Title;
+                if ( support.GetType() == typeof(Security))
+                {
+                    message += $" Priority Personnel";
+                }
+                return message;
+            }
+            else
+            {
+                return "Too Important for a Security Pass";
+            }
+        }
+    }
+
+    /**** Please do not alter the code below ****/
+
+    public interface TeamSupport { string Title { get; } }
+
+    public abstract class Staff : TeamSupport { public abstract string Title { get; } }
+
+    public class Manager : TeamSupport { public string Title { get; } = "The Manager"; }
+
+    public class Chairman : TeamSupport { public string Title { get; } = "The Chairman"; }
+
+    public class Physio : Staff { public override string Title { get; } = "The Physio"; }
+
+    public class OffensiveCoach : Staff { public override string Title { get; } = "Offensive Coach"; }
+
+    public class GoalKeepingCoach : Staff { public override string Title { get; } = "Goal Keeping Coach"; }
+
+    public class Security : Staff { public override string Title { get; } = "Security Team Member"; }
+
+    public class SecurityJunior : Security { public override string Title { get; } = "Security Junior"; }
+
+    public class SecurityIntern : Security { public override string Title { get; } = "Security Intern"; }
+
+    public class PoliceLiaison : Security { public override string Title { get; } = "Police Liaison Officer"; }
+}
---
+++ 27_Overflow.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Code
+{
+    public static class CentralBank
+    {
+        public static string DisplayDenomination(long @base, long multiplier)
+        {
+            try
+            {
+                long result = 0;
+                checked { result = @base * multiplier; }
+                return result.ToString();
+            } catch (OverflowException ex) {
+                return "*** Too Big ***";
+            }
+        }
+
+        public static string DisplayGDP(float @base, float multiplier)
+        {
+            try
+            {
+                float result = 0;
+                checked { result = @base * multiplier; }
+                if (result == float.NegativeInfinity ||  result == float.PositiveInfinity) { throw new OverflowException(); }
+                return result.ToString();
+            }
+            catch (OverflowException ex)
+            {
+                return "*** Too Big ***";
+            }
+        }
+
+        public static string DisplayChiefEconomistSalary(decimal salaryBase, decimal multiplier)
+        {
+            try
+            {
+                decimal result = 0;
+                checked { result = salaryBase * multiplier; }
+                return result.ToString();
+            }
+            catch (OverflowException ex)
+            {
+                return "*** Much Too Big ***";
+            }
+        }
+
+        // ESTO SERÍA SI LO HICIESEMOS TODO CON LA MISMA FUNCIÓN
+        //public static string DisplayValue<T>(T @base, T multiplier) where T : struct, IComparable, IConvertible, IFormattable
+        //{
+        //    try
+        //    {
+        //        dynamic baseValue = @base;
+        //        dynamic multiplierValue = multiplier;
+        //        dynamic result = baseValue * multiplierValue;
+        //        return result.ToString();
+        //    }
+        //    catch (OverflowException)
+        //    {
+        //        return "*** Too Big ***";
+        //    }
+        //}
+    }
+}
