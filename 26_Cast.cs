06b0c40bcf9ed381483a797546a6783deebf4b24
vhidalgo <->
2024-10-02 10:15:14 +0200

    Resuelto ejercicio de teoría sobre las Propiedades de las clases y cómo hacer get y set de las mismas

--- 26_Cast.cs
+++ 26_Cast.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Code
+namespace Code2
 {
     public class SecurityPassMaker
     {
---
+++ 28_Properties.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Code
+{
+    class WeighingMachine
+    {
+        public int Precision { get; }
+        private double _weight;
+        public double TareAdjustment { get; set; } = 5;
+
+        public double Weight
+        {
+            get { return _weight; }
+
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentOutOfRangeException("Weight can´t be negative.");
+                }
+                else
+                {
+                    _weight = value;
+                }
+            }
+        }
+
+        public WeighingMachine(int precision)
+        {
+            Precision = precision;
+        }
+        
+
+        public string DisplayWeight
+        {
+            get { return $"{Math.Round(_weight -  TareAdjustment,Precision).ToString($"F{Precision}")} kg";  }
+        }
+    }
+}
