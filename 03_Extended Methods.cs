8db21a06b6df23de346303e41fab5be4f8453adf
vhidalgo <->
2024-10-09 09:50:51 +0200

    Resueltos todos los ejercicios restantes sobre Extension Methods.

--- 03_Extended Methods.cs
+++ 03_Extended Methods.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using Code;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Security.AccessControl;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -33,6 +36,7 @@ namespace Test
         public static string LogLevel(this string originalString) => originalString.Substring(1, originalString.IndexOf(')')-1);
     }
 
+    //-----------------------------------------------------------------------------------------------------------------------------------------------------
 
     public static class RomanNumeralExtension
     {
@@ -101,6 +105,8 @@ namespace Test
         }
     }
 
+    //-----------------------------------------------------------------------------------------------------------------------------------------------------
+
     public static class AccumulateExtensions
     {
         public static IEnumerable<U> Accumulate<T, U>(this IEnumerable<T> collection, Func<T, U> func)
@@ -117,5 +123,146 @@ namespace Test
             return result;
         }
     }
+
+    //-----------------------------------------------------------------------------------------------------------------------------------------------------
+
+    public static class RealNumberExtension
+    {
+        public static double Expreal(this int realNumber, RationalNumber r) => Math.Pow(Math.Pow((double)realNumber, r.num), 1.0/r.den);
+    }
+
+    public struct RationalNumber
+    {
+        public int num;
+        public int den;
+        public RationalNumber(int numerator, int denominator)
+        {
+            num = numerator;
+            den = denominator;
+        }
+
+        public static RationalNumber operator +(RationalNumber r1, RationalNumber r2)
+        {
+            int newNum = (r1.num * r2.den + r1.den*r2.num);
+            int newDen = r1.den * r2.den;
+            if (newDen == 0) throw new ArgumentException("Can´t divide by 0.");
+            return new RationalNumber(newNum, newDen).Reduce();
+        }
+
+        public static RationalNumber operator -(RationalNumber r1, RationalNumber r2)
+        {
+            int newNum = (r1.num * r2.den - r1.den*r2.num);
+            int newDen = r1.den * r2.den;
+            if (newDen == 0) throw new ArgumentException("Can´t divide by 0.");
+            return new RationalNumber(newNum, newDen).Reduce();
+        }
+
+        public static RationalNumber operator *(RationalNumber r1, RationalNumber r2)
+        {
+            int newNum = r1.num * r2.num;
+            int newDen = r1.den * r2.den;
+            if (newDen == 0) throw new ArgumentException("Can´t divide by 0.");
+            return new RationalNumber(newNum, newDen).Reduce();
+        }
+
+        public static RationalNumber operator /(RationalNumber r1, RationalNumber r2)
+        {
+            int newNum = r1.num * r2.den;
+            int newDen = r1.den * r2.num;
+            if (newDen == 0) throw new ArgumentException("Can´t divide by 0.");
+            return new RationalNumber(newNum, newDen).Reduce();
+        }
+
+        public RationalNumber Abs() => new RationalNumber(Math.Abs(num), Math.Abs(den)).Reduce();
+
+        public RationalNumber Reduce()
+        {
+            int MCD = MaxCommonDivisor(num, den);
+            RationalNumber result = den < 0?
+                new RationalNumber(-num/MCD, -den/MCD) :
+                new RationalNumber(num/MCD, den/MCD);
+            return result;
+        }
+
+        public RationalNumber Exprational(int power) => power< 0 ?
+                new RationalNumber((int)Math.Pow(den, -power), (int)Math.Pow(num, -power)).Reduce() :
+                new RationalNumber((int)Math.Pow(num, power), (int)Math.Pow(den, power)).Reduce();
+
+
+        public double Expreal(int baseNumber) => Math.Pow(baseNumber^num,1/den);
+
+        private static int MaxCommonDivisor(int num, int den)
+        {
+            for (int i = Math.Max(Math.Abs(num), Math.Abs(den)); i >= 1;i--) 
+            { 
+                if (num % i == 0 && den % i == 0) return i;
+            }
+            return -1;
+        }
+    }
+
+
+    //-----------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+    public struct ComplexNumber
+    {
+        private double real;
+        private double imag;
+
+        public ComplexNumber(int real)
+        {
+            this.real = real;
+            this.imag = 0;
+        }
+
+        public ComplexNumber(double real, double imaginary)
+        {
+            this.real = real;
+            this.imag = imaginary;
+        }
+
+        public double Real() => this.real;
+
+        public double Imaginary() => this.imag;
+
+        public ComplexNumber Mul(ComplexNumber other) => 
+            new ComplexNumber(
+                real*other.real - imag*other.imag, 
+                imag*other.real + real * other.imag
+            );
+
+        public ComplexNumber Mul(int other) => Mul(new ComplexNumber(other,0));
+
+        public ComplexNumber Add(ComplexNumber other) =>
+            new ComplexNumber(
+                real + other.real,
+                imag + other.imag
+            );
+        public ComplexNumber Add(int other) => Add(new ComplexNumber(other,0));
+
+        public ComplexNumber Sub(ComplexNumber other) =>
+            new ComplexNumber(
+                real - other.real,
+                imag - other.imag
+            );
+
+        public ComplexNumber Sub(int other) => Sub(new ComplexNumber(other, 0));
+
+        public ComplexNumber Div(ComplexNumber other) =>
+            new ComplexNumber(
+                (real*other.real + imag*other.imag)/(Math.Pow(other.real,2) + Math.Pow(other.imag,2)),
+                (imag*other.real - real * other.imag)/(Math.Pow(other.real, 2) + Math.Pow(other.imag, 2))
+            );
+
+        public ComplexNumber Div(int other) => Div(new ComplexNumber(other, 0));
+
+        public double Abs() => Math.Sqrt(Math.Pow(real,2)+Math.Pow(imag,2));
+
+        public ComplexNumber Conjugate() => new ComplexNumber(real,-imag);
+
+        public ComplexNumber Exp() => new ComplexNumber(Math.Exp(real)*Math.Cos(imag), Math.Exp(real)*Math.Sin(imag));
+    }
+
 
 }
