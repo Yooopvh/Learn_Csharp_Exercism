6cbbe0251225c0f28a5f68e0570e90ee7ab1a22d
vhidalgo <->
2024-10-15 08:15:27 +0200

    Resuelto ejercicio sobre cifrado

--- 07_Randomness.cs
+++ 07_Randomness.cs
@@ -52,4 +52,59 @@ namespace Test
             return (GpowA % primeP);
         }
     }
+
+    //-----------------------------------------------------------------------------------------------------------------------------------------------
+
+    public class SimpleCipher
+    {
+        private string _key = "";
+        public SimpleCipher()
+        {
+            Random rnd= new Random();
+            for (int i = 0; i < 100; i++)
+            {
+                _key += (char)(Math.Floor(rnd.NextDouble() * 26) + 'a');
+            }
+        }
+
+        public SimpleCipher(string key) => _key = key;
+
+        public string Key { get => _key; }
+
+        public string Encode(string plaintext)
+        {
+            string result = "";
+            int keyIndex = 0;
+            foreach (char c in plaintext)
+            {
+                if (c < 'a' || c > 'z') throw new ArgumentException();  
+                char keyChar = _key[keyIndex];
+                char newChar = (char)(c + (keyChar - 'a'));
+                char newLetter = newChar > 'z' ? (char) (newChar - 'z' + 'a'-1) : newChar;
+                result += newLetter;
+
+                keyIndex++;
+                if (keyIndex == _key.Length) keyIndex = 0;
+            }
+            return result;
+        }
+
+        public string Decode(string ciphertext)
+        {
+            string result = "";
+            int keyIndex = 0;
+            foreach (char c in ciphertext)
+            {
+                if (c < 'a' || c > 'z') throw new ArgumentException();
+                char keyChar = _key[keyIndex];
+                char newChar = (char)(c - (keyChar - 'a'));
+                char newLetter = newChar < 'a' ? (char)(newChar + 'z' - 'a'+1) : newChar;
+                result += newLetter;
+
+                keyIndex++;
+                if (keyIndex == _key.Length) keyIndex = 0;
+            }
+            return result;
+        }
+    }
 }
