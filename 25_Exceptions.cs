e70d18c831e98dfbcdb29116bb7976ef0eea935a
vhidalgo <->
2025-01-03 11:00:32 +0100

    Resuelto ejercicio sobre excepciones con programación dinámica

--- 25_Exceptions.cs
+++ 25_Exceptions.cs
@@ -58,5 +58,41 @@ namespace Code
         //            _ => throw new ArgumentOutOfRangeException(),
         //        };
         //}
+
+        //-------------------------------------------------------------------------------------------------------------------------------------------------------
+
+        public static class Change
+        {
+
+            public static int[] FindFewestCoins(int[] coins, int target)
+            {
+                int pendingChange = target;
+                if (target < 0) throw new ArgumentException();
+
+                int[] dp = new int[target + 1]; //Almacena el número de monedas necesarias para alcanzar esa cantidad
+                dp = dp.Select(x => int.MaxValue ).ToArray();
+                dp[0] = 0;
+                List<int>[] dp_coinsCombination = Enumerable.Range(0, dp.Length)
+                                              .Select(x => new List<int>())
+                                              .ToArray(); //Tabla para almacenar la combinación de monedas
+
+                for (int i = 1; i < dp.Length; i++) {
+                    foreach (int coin in coins)
+                    {
+                        if (i >= coin && dp[i - coin] < dp[i])
+                        {
+                            dp[i] = dp[i - coin] + 1;
+                            dp_coinsCombination[i] = dp_coinsCombination[i-coin].Count > 0 ?  new List<int> (dp_coinsCombination[i - coin]) : new List<int>();
+                            dp_coinsCombination[i].Add(coin);
+                        }
+                    }
+                };
+
+                if (target - dp_coinsCombination.Last().Sum() > 0) throw new ArgumentException();
+
+                return dp_coinsCombination.Last().OrderBy(x=>x).ToArray();
+
+            }
+        }
     }
 }
